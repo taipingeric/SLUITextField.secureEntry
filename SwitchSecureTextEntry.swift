19bf0571278ba74fd465d8396f9f2bfacd5b66ea
SaiCyLi <taipingeric@gmail.com>
2016-05-16 11:42:36 +0800

    re-indent

--- SwitchSecureTextEntry.swift
+++ SwitchSecureTextEntry.swift
@@ -1,26 +1,18 @@
-//
-//  File.swift
-//  test
-//
-//  Created by ChihyangLi. on 5/16/16.
-//  Copyright © 2016 ChihyangLi. All rights reserved.
-//
-
 import UIKit
 
 extension UITextField {
-
-	func switchSecureTextEntry(isSecure: Bool? = nil, normalFont: UIFont? = nil, secureFont: UIFont? = nil) {
-		
-		let defaultFont = UIFont.systemFontOfSize(14)
-		self.secureTextEntry = isSecure ?? !self.secureTextEntry
-		
-		self.font = nil
-		if secureTextEntry {
-			self.font = secureFont ?? defaultFont
-		} else {
-			self.font = normalFont ?? defaultFont
-		}
-	}
-	
+    
+    func switchSecureTextEntry(isSecure: Bool? = nil, normalFont: UIFont? = nil, secureFont: UIFont? = nil) {
+        
+        let defaultFont = UIFont.systemFontOfSize(14)
+        self.secureTextEntry = isSecure ?? !self.secureTextEntry
+        
+        self.font = nil
+        if secureTextEntry {
+            self.font = secureFont ?? defaultFont
+        } else {
+            self.font = normalFont ?? defaultFont
+        }
+    }
+    
 }
